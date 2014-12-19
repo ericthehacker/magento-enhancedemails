7ec1cb1bd660edde238dab48ebd6673b579ffb2c
Eric Wiese <eric.wiese@classyllama.com>
2014-12-18 18:34:46 -0600

    Add event to Mage_Core_Model_Email_Template:: getTemplateFilter() via rewrite

---
+++ app/code/community/EW/EnhancedEmails/Model/Core/Email/Template.php
@@ -0,0 +1,27 @@
+<?php
+
+class EW_EnhancedEmails_Model_Core_Email_Template extends Mage_Core_Model_Email_Template
+{
+    protected $_eventPrefix = 'core_email_template';
+
+    /**
+     * Get filter object for template processing logic
+     * and dispatch event
+     *
+     * @return Mage_Core_Model_Email_Template_Filter
+     */
+    public function getTemplateFilter()
+    {
+        $templateFilter = parent::getTemplateFilter();
+
+        Mage::dispatchEvent(
+            $this->_eventPrefix . '_get_template_filter_after',
+            array(
+                'template_filter'   => $templateFilter,
+                'template'          => $this
+            )
+        );
+
+        return $templateFilter;
+    }
+}
